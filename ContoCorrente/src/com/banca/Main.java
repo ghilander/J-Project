7dd09da5d11e825ec9a9ee4a056d1d0cd9c3f6fb
Davide Ghion <davide.ghion@e-projectsrl.it>
2016-04-20 09:53:00 +0200

    Aggiunto proprietario sulla classe Account

--- ContoCorrente/src/com/banca/Main.java
+++ ContoCorrente/src/com/banca/Main.java
@@ -2,8 +2,9 @@ package com.banca;
 
 public class Main {
 	public static void main(String[] args){
-		Account conto = new Account(30);
+		Account conto = new Account(30, "Davide");
 		
 		System.out.println("Il tuo conto è di: "+conto.getConto());
+		System.out.println("Il proprietario è: "+conto.getProperty());
 	}
 }
